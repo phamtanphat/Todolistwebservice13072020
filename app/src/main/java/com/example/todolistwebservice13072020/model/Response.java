bf7aed35cd2e830c6261457320a29eba3ce1e6c0
phatdroid94 <phatdroid94@gmail.com>
2020-10-12 19:07:29 +0700

    reponse

---
+++ app/src/main/java/com/example/todolistwebservice13072020/model/Response.java
@@ -0,0 +1,56 @@
+package com.example.todolistwebservice13072020.model;
+
+import java.util.List;
+
+public class Response <T> {
+
+    private int totalpage;
+    private String currentpage;
+    private boolean success;
+    private String message;
+    private T data;
+
+    public Response() {
+    }
+
+    public int getTotalpage() {
+        return totalpage;
+    }
+
+    public void setTotalpage(int totalpage) {
+        this.totalpage = totalpage;
+    }
+
+    public String getCurrentpage() {
+        return currentpage;
+    }
+
+    public void setCurrentpage(String currentpage) {
+        this.currentpage = currentpage;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
