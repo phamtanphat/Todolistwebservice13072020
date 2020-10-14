2cad49799cff6052b8734cb48fe6336a97c939da
phatdroid94 <phatdroid94@gmail.com>
2020-10-14 17:59:39 +0700

    get word from page

--- app/src/main/java/com/example/todolistwebservice13072020/view/MainActivity.java
+++ app/src/main/java/com/example/todolistwebservice13072020/view/MainActivity.java
@@ -1,16 +1,34 @@
 package com.example.todolistwebservice13072020.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.todolistwebservice13072020.R;
+import com.example.todolistwebservice13072020.model.Response;
+import com.example.todolistwebservice13072020.model.Word;
+import com.example.todolistwebservice13072020.viewmodel.WordViewModel;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    WordViewModel mWordViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mWordViewModel = new WordViewModel();
+
+        mWordViewModel.getWordsFromPage().observe(this, new Observer<Response<List<Word>>>() {
+            @Override
+            public void onChanged(Response<List<Word>> listResponse) {
+                Log.d("BBB",listResponse.getData().size() + "");
+            }
+        });
+
+        mWordViewModel.callWordFromPage(1 , 5);
     }
 }
